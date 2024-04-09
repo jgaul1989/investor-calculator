6ba70bb5e9310390996e635a70b226a99b6f2f52
Jonathan <jonathan.gaul1@gmail.com>
2024-04-09 17:21:32 -0400

    Added function to select data from the companies table whose name matches the search string

--- crud/operations.py
+++ crud/operations.py
@@ -1,4 +1,6 @@
 from sqlalchemy.orm import Session
+from sqlalchemy import select
+from models.company import Company
 
 
 def insert_data(engine, model_class, data):
@@ -7,3 +9,14 @@ def insert_data(engine, model_class, data):
             instance = model_class(**record)
             session.add(instance)
         session.commit()
+
+
+def select_data_from_company(engine, search_string):
+    with Session(engine) as session:
+        # Construct a query to select companies where the name contains `search_string`
+        stmt = select(Company).where(Company.name.like(f"%{search_string}%"))
+        result = session.execute(stmt)
+
+        # Fetch all results
+        companies = result.scalars().all()
+        return companies
