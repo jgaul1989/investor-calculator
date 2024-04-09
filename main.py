49042058f342455c1c351ebc49af4356ac8ce151
Jonathan <jonathan.gaul1@gmail.com>
2024-04-09 16:05:41 -0400

    Added create company function to add a company to the database

--- main.py
+++ main.py
@@ -42,13 +42,50 @@ def top_ten_operations():
         print("Invalid option!")
 
 
-def crud_operations():
+def create_company(engine):
+    print("Enter ticker (in the format 'MOON'):")
+    ticker = input("")
+    print("Enter company (in the format 'Moon Corp'):")
+    name = input("")
+    print("Enter industries (in the format 'Technology'):")
+    sector = input("")
+    print("Enter ebitda (in the format '987654321'):")
+    ebitda = float(input(""))
+    print("Enter sales (in the format '987654321'):")
+    sales = float(input(""))
+    print("Enter net profit (in the format '987654321'):")
+    net_profit = float(input(""))
+    print("Enter market price (in the format '987654321'):")
+    market_price = float(input(""))
+    print("Enter net debt (in the format '987654321'):")
+    net_debt = float(input(""))
+    print("Enter assets (in the format '987654321'):")
+    assets = float(input(""))
+    print("Enter equity (in the format '987654321'):")
+    equity = float(input(""))
+    print("Enter cash equivalents (in the format '987654321'):")
+    cash_equivalents = float(input(""))
+    print("Enter liabilities (in the format '987654321'):")
+    liabilities = float(input(""))
+
+    company = [{"ticker": ticker, "name": name, "sector": sector}]
+    financial = [{"ticker": ticker, "ebitda": ebitda, "sales": sales, "net_profit": net_profit,
+                  "market_price": market_price, "net_debt": net_debt, "assets": assets, "equity": equity,
+                  "cash_equivalents": cash_equivalents, "liabilities": liabilities}]
+    insert_data(engine, Company, company)
+    insert_data(engine, Financial, financial)
+    print("Company created successfully!")
+
+
+def crud_operations_menu(engine):
     show_crud_menu()
     print("Enter an option:")
     crud_operation = input("")
 
-    if crud_operation in ["0", "1", "2", "3", "4", "5"]:
+    if crud_operation in ["0", "2", "3", "4", "5"]:
         print("Not implemented!")
+    elif crud_operation == "1":
+        create_company(engine)
     else:
         print("Invalid option!")
 
@@ -66,7 +103,7 @@ def import_data(engine):
 if __name__ == "__main__":
     engine = create_engine('sqlite:///investor.db')
     Base.metadata.create_all(engine)
-    import_data(engine)
+    # import_data(engine)
 
     user_command = ""
 
@@ -77,7 +114,7 @@ if __name__ == "__main__":
         user_command = input("")
 
         if user_command == "1":
-            crud_operations()
+            crud_operations_menu(engine)
         elif user_command == "2":
             top_ten_operations()
         elif user_command not in ["0", "1", "2"]:
